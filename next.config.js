794fce782eb4c2b0cb53557a217e7c996e25a62a
Luis Rivera <l.riveraa664@hotmail.com>
2023-12-03 15:58:41 -0300

    v.0.01

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['i.postimg.cc'], // Agrega el dominio de tus imágenes aquí
+      domains: ['i.postimg.cc' , 'photos.app.goo.gl'],// Agrega el dominio de tus imágenes aquí
     },
     // ... otras configuraciones de Next.js, si las tienes
   };
