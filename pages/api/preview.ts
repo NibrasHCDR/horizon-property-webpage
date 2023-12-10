72899d39a411e71510f567480cf07b393b22afda
Luis Rivera <l.riveraa664@hotmail.com>
2023-12-10 02:16:15 -0300

    first commit

--- pages/api/preview.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
-
-export default function preview(req: NextApiRequest, res:NextApiResponse) {
-        res.setPreviewData({});
-        res.writeHead(307, { Location: "/"});
-        res.end();
-
-    }
